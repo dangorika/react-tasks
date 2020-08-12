f2d476fe8ff8e9c04ad7e89892f5b30ff5ffd3c4
dangorika <olha.yev@gmail.com>
2020-08-12 22:25:00 +0300

    feature/Form: Add input for adding new items in todo list

--- 01.Todo/11-states-and-events/src/components/item-add-form/item-add-form.js
+++ 01.Todo/11-states-and-events/src/components/item-add-form/item-add-form.js
@@ -4,16 +4,38 @@ import './item-add-form.sass';
 
 export default class ItemAddForm extends Component {
 
-  render() {
-    const { onAdded } = this.props;
+  state = {
+    label: ''
+  };
 
+  onLabelChange = e => {
+    this.setState({
+      label: e.target.value
+    });
+  };
+
+  onSubmit = e => {
+    e.preventDefault();
+    
+    const { onAdded } = this.props;
+    const { label } = this.state;
+
+    onAdded(label);
+    e.target.reset();
+  };
+
+  render() {
     return (
-      <div className="item-add-form">
-        <button className="btn btn-secondary"
-                onClick={() => onAdded('Hello world')}>
+      <form className="item-add-form d-flex"
+            onSubmit={this.onSubmit}>
+        <input type="text"
+               className="form-control"
+               onChange={this.onLabelChange}
+               placeholder="What needs to be done" />
+        <button className="btn btn-secondary">
           Add Item
         </button>
-      </div>
+      </form>
     );
   }
 }
