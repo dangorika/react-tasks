fbf66c75a7cf997b37dbab8c0f407a73b08bcd28
dangorika <olha.yev@gmail.com>
2020-08-18 16:36:41 +0300

    feature/Fetch: Test fetch api

--- 02.StarDB/01-star-project/src/index.js
+++ 02.StarDB/01-star-project/src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello world');
+const getResource = async url => {
+  const response = await fetch(url);
+  const result = await response.json();
+
+  return result;
+};
+
+getResource('https://swapi.dev/api/people/1/')
+  .then(result => {
+    console.log(result);
+  });
