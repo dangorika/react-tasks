1c5d16759f5bcba301b5848783671e2bd275683c
dangorika <olha.yev@gmail.com>
2020-08-18 16:47:23 +0300

    feature/FetchErrors: Catch errors in fetch api

--- 02.StarDB/01-star-project/src/index.js
+++ 02.StarDB/01-star-project/src/index.js
@@ -1,11 +1,17 @@
 const getResource = async url => {
   const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`Could not fetch ${url}, received ${response.status}`);
+  }
   const result = await response.json();
 
   return result;
 };
 
-getResource('https://swapi.dev/api/people/1/')
+getResource('https://swapi.dev/api/people/134243/')
   .then(result => {
     console.log(result);
+  })
+  .catch(error => {
+    console.log(error);
   });
