6f7fda89f847e0fd86edbd43a8228688db058bb0
dangorika <olha.yev@gmail.com>
2020-08-21 17:04:59 +0300

    feature/ComponentDidCatchPractice: Refactor people page

--- 02.StarDB/02-components-life-cycle/src/components/app/app.js
+++ 02.StarDB/02-components-life-cycle/src/components/app/app.js
@@ -1,24 +1,18 @@
 import React, { Component } from 'react';
 import Header from 'components/header';
 import RandomPlanet from 'components/random-planet';
-import ItemList from 'components/item-list';
-import PersonDetails from 'components/person-details';
+import PeoplePage from 'components/people-page';
 import ErrorButton from 'components/error-button';
 import ErrorMessage from 'components/error-message';
 
 import './app.sass';
 
+
 export default class App extends Component {
 
   state = {
-    selectedPerson: 1,
+
     hasError: false
-  };
-  
-  onPersonSelected = id => {
-    this.setState({
-      selectedPerson: id
-    });
   };
 
   componentDidCatch() {
@@ -54,11 +48,10 @@ export default class App extends Component {
           </div>
         </div>
         <div className="app__row row">
-          <div className="app__col col-sm-6">
-            <ItemList onItemSelected={this.onPersonSelected} />
-          </div>
-          <div className="app__col col-sm-6">
-            <PersonDetails personId={this.state.selectedPerson} />
+          <div className="col-sm-12">
+            <PeoplePage />
+            <PeoplePage />
+            <PeoplePage />
           </div>
         </div>
       </div>
