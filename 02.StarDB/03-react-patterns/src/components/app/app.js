047f167d657fa9e7ad211c2cbfafd2567e4ab371
Olha Yevtushenko <olya@coderiver.com.ua>
2020-11-22 13:48:55 +0200

    feature/Context: Created SwapiService Provider and Consumer

--- 02.StarDB/03-react-patterns/src/components/app/app.js
+++ 02.StarDB/03-react-patterns/src/components/app/app.js
@@ -8,32 +8,36 @@ import {
   PersonDetails,
   PlanetDetails,
   StarshipDetails
-} from '../sw-components';
-
-import './app.sass';
-import SwapiService from 'services/swapi-service';
+} from 'components/sw-components';
+import { SwapiServiceProvider } from 'components/swapi-service-context';
 import ErrorBoundry from 'components/error-boundry';
 import PageRow from 'components/page-row';
 
+import './app.sass';
+import SwapiService from 'services/swapi-service';
+import DummySwapiService from 'services/dummy-swapi-service';
+
 export default class App extends Component {
 
-  swapiService = new SwapiService();
+  swapiService = new DummySwapiService();
 
   render() {
     return (
       <ErrorBoundry>
-        <div className="app container">
-          <Header />
+        <SwapiServiceProvider value={this.swapiService}>
+          <div className="app container">
+            <Header />
 
-          <PersonDetails itemId={11} />
-          <PlanetDetails itemId={5} />
-          <StarshipDetails itemId={9} />
+            <PersonDetails itemId={11} />
+            <PlanetDetails itemId={5} />
+            <StarshipDetails itemId={9} />
 
-          <PersonList />
-          <PlanetList />
-          <StarshipList />
+            <PersonList />
+            <PlanetList />
+            <StarshipList />
 
-        </div>
+          </div>
+        </SwapiServiceProvider>
       </ErrorBoundry>
     );
   }
