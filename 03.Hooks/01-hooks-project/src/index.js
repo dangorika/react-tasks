15aec497001f342eb3b19e1c664d74b7d8f62cf3
Olha Yevtushenko <olya@coderiver.com.ua>
2020-12-13 12:44:34 +0200

    feature/Hooks: Added setState hook

--- 03.Hooks/01-hooks-project/src/index.js
+++ 03.Hooks/01-hooks-project/src/index.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => <p>Hi</p>;
+const App = () => (
+  <div>
+    <HookSwitcher />
+  </div>
+);
+
+const HookSwitcher = () => {
+
+  const [color, setColor] = useState('black');
+  const [fontSize, setFontSize] = useState(14);
+
+  return (
+    <div style={{ padding: '10px', backgroundColor: color, fontSize: `${fontSize}px`, color: 'gray' }}>
+      Hello world
+      <button onClick={() => setColor('black')}>Dark</button>
+      <button onClick={() => setColor('white')}>Light</button>
+      <button onClick={() => setFontSize((s) => s + 2)}>Increase font size</button>
+      <button onClick={() => setFontSize((s) => s - 2)}>Descrease font size</button>
+    </div>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
